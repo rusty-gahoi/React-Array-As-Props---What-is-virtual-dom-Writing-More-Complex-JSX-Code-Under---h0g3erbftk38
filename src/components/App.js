c48183b8ce150fa3d7fe469cb3aa74bf9c6ff294
rusty-gahoi <vipulrocks28@gmail.com>
2022-11-24 17:14:22 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"
 const Join = (props) =>{
   return(
     <div id ="join">
-      {/* Access prop 'words' and print it using .join like words.join(',')*/}
+      {/* Access prop 'words' and print it using .join like words.join(',') */}
+      <p>{props.words.join(",")}</p>
     </div>
   )
 }
@@ -12,7 +13,7 @@ const App = () => {
 
   return (
     <div id="main">
-      <Join  />
+      <Join words = {arr} />
     </div>
   )
 }
